150b7775c6d58c98369d4591431f4582dc58fb7e
brunkb <brunkb@gmail.com>
2014-11-19 15:18:04 -0600

    Create angular app using Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherEyeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherEyeApp
+ */
+angular.module('weatherEyeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
